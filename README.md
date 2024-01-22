5040921c301f689f48b3c2f9fd468435f87a89bf
dengying <1174915260@qq.com>
2024-01-22 20:09:37 +0800

    deploy

--- README.md
+++
@@ -1,4 +0,0 @@
-# dy-virtual-table-doc
-虚拟表格文档使用说明
-
-文档地址：https://clyingdeng.github.io/dy-virtual-table-doc
--- src/main.ts
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-// import DyVirtualTable from 'dy-virtual-table'
-// import 'dy-virtual-table/dist/style.css'
-createApp(App).mount('#app')
